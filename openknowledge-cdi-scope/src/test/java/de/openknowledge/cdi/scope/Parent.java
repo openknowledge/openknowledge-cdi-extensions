14e316913d12bb7c5f4003df1d1100a76f5dad19
jens.schumann <jens.schumann@openknowledge.de>
2012-08-04 00:03:36 +0200

    Removed year in copyright.

--- openknowledge-cdi-scope/src/test/java/de/openknowledge/cdi/scope/Parent.java
+++ openknowledge-cdi-scope/src/test/java/de/openknowledge/cdi/scope/Parent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011 open knowledge GmbH
+ * Copyright open knowledge GmbH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
